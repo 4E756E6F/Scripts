e91bb94619166cce7bcd1cd8302848c3a954cf37
Nuno Santos <61665536+4E756E6F@users.noreply.github.com>
2023-03-15 12:33:06 +0000

    Updated syntax
    Showing comand not found error, do to syntax change

--- Domain_Add.sh
+++ Domain_Add.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 echo "This script will help you to add a Linux based OS to your Windows Active Directory domain"
 echo """With the release of Ubuntu 20.04 and Debian 11 the
 method to add a pc, running one of this OS,
 to a Windows Active Diretory domain as changed..
 So with this in mind let me now witch one are you using"""
-sleep 6
+sleep 4
 echo ""
 echo ""
 echo "If you're using Ubuntu < 20.04 or Debian < 11"
@@ -29,9 +31,8 @@ echo "#                                                      #"
 echo "#                                                      #"
 echo "#                                                      #"
 echo "########################################################"
-sleep 10
-if $OS == 0
-then
+sleep 6
+if [ $OS == 0 ]; then
     sudo apt-get install -y ssh
     wget http://download.beyondtrust.com/PBISO/8.0.1/linux.deb.x64/pbis-open-8.0.1.2029.linux.x86_64.deb.sh
     sudo chmod +x pbis-open-8.0.1.2029.linux.x86_64.deb.sh
@@ -46,8 +47,7 @@ then
     sudo rm pbis-open-8.0.1.2029.linux.x86_64.deb.sh
     sudo rm -r pbis-open-8.0.1.2029.linux.x86_64.deb/
     domainjoin-cli query
-elif $OS == 1
-then
+elif [ $OS == 1 ]; then
     sudo apt install -y ssh
     sudo wget https://github.com/BeyondTrust/pbis-open/releases/download/9.1.0/pbis-open-9.1.0.551.linux.x86_64.deb.sh --no-check-certificate
     sudo chmod +x pbis-open-9.1.0.551.linux.x86_64.deb.sh
